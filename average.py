550ca6505e67b9c2d0450c5778ffb9a40ef84f5f
alicegodden <136358959+alicegodden@users.noreply.github.com>
2024-03-06 10:35:50 +0000

    Add files via upload

---
+++ average.py
@@ -0,0 +1,37 @@
+# put rows in numerical order
+# Read the content of merged_output.txt
+with open('merged_output.txt', 'r') as merged_file:
+    lines = merged_file.readlines()
+
+# Filter lines starting with a number
+number_lines = [line.strip() for line in lines if line.strip().startswith(tuple(map(str, range(10))))]
+
+# Sort the lines numerically
+sorted_lines = sorted(number_lines, key=lambda x: int(x.split()[0]))
+
+# Write the sorted lines to order_output.txt
+with open('order_output.txt', 'w') as output_file:
+    output_file.write('\n'.join(sorted_lines))
+
+
+# calculate averages
+
+from collections import defaultdict
+
+# Read the content of order_output.txt
+with open('order_output.txt', 'r') as order_file:
+    lines = order_file.readlines()
+
+# Group lines by their starting number
+grouped_lines = defaultdict(list)
+for line in lines:
+    num, value = line.strip().split()
+    grouped_lines[num].append(float(value))
+
+# Calculate the average for each group
+averages = {num: sum(values) / len(values) for num, values in grouped_lines.items()}
+
+# Write the averages to a new file
+with open('averages_output.txt', 'w') as output_file:
+    for num, avg in averages.items():
+        output_file.write(f"{num} {avg:.2f}\n")
---
+++ coverage.py
@@ -0,0 +1,37 @@
+# plot the average coverage of each chromosome in a plot
+# import packages
+import matplotlib.pyplot as plt
+
+
+# Read data from averages_output.txt
+with open('averages_output.txt', 'r') as input_file:
+    lines = input_file.readlines()
+
+# Extract x and y values
+x = []
+y = []
+for line in lines:
+    num, value = line.strip().split()
+    x.append(int(num))
+    y.append(float(value))
+
+# Plot the data
+plt.figure(figsize=(10, 6))
+plt.plot(x, y, marker='o', linestyle='none', color='red', alpha=0.6)
+plt.xlabel('Chromosome', weight='bold')
+plt.ylabel('Coverage', weight='bold')
+plt.title('Average Coverage per Chromosome', weight='bold')
+plt.yticks(weight='bold')
+plt.xticks(weight='bold')
+plt.grid(linewidth=0.25)
+# Set x-axis tick marks and labels
+plt.xticks(range(min(x), max(x) + 1))
+# set y axis range
+plt.ylim(50, 60)  # Adjust the range as needed
+
+# Save the plot as PNG
+plt.savefig('coverage_plot_HS_gDNA_all_samples.png', dpi=600)
+plt.savefig('coverage_plot_HS_gDNA_all_samples.pdf')
+
+#show the plot
+plt.show()
