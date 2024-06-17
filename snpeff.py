5440b168af35b9daef39c6674ee23ba0f512f3f3
Dr. Alice Godden <136358959+alicegodden@users.noreply.github.com>
2024-06-17 19:16:53 +0100

    Update snpeff.py

--- snpeff.py
+++ snpeff.py
@@ -1,8 +1,7 @@
-# Title- snpeff.py
-# Author - Dr. Alice Godden
-# Import packages
 import matplotlib.pyplot as plt
 import pysam
+import seaborn as sns
+from matplotlib.patches import Ellipse
 
 def extract_chrom_pos_from_vcf(vcf_file):
     chrom_pos_dict = {str(i): [] for i in range(1, 26)}
@@ -14,7 +13,6 @@ def extract_chrom_pos_from_vcf(vcf_file):
                 chrom_pos_dict[chromosome].append(position)
     return chrom_pos_dict
 
-
 def get_chrom_end(chrom):
     with open('chrom_end.txt') as f:
         for line in f:
@@ -22,7 +20,6 @@ def get_chrom_end(chrom):
             if chrom_name == chrom:
                 return int(end_pos)
     return None
-
 
 # Replace 'your_vcf_file.vcf.gz' with the path to your VCF file or .vcf.gz
 vcf_file = 'high_filtered_sift_CC_snpeff_female_allchrs.eff.vcf'
@@ -42,7 +39,6 @@ with open('genes.txt') as f:
         chrom, pos, gene = line.split()
         genes_data.append((chrom, int(pos), gene))
 
-
 # Read the chrom_end.txt file
 end_data = []
 with open('chrom_end.txt') as f:
@@ -50,42 +46,53 @@ with open('chrom_end.txt') as f:
         chrom, pos = line.split()
         end_data.append((chrom, int(pos)))
 
+# Color palette for chromosomes
+palette = sns.color_palette("rocket", n_colors=len(chrom_pos_dict))
 
 # Create the plot
 fig, ax = plt.subplots(figsize=(15, 8))
-for chromosome, positions in chrom_pos_dict.items():
-    plt.scatter([chromosome] * len(positions), positions, label='Chromosome' + chromosome)
 
-# Add the chrcen data as marks on the plot
+# Plot chromosome positions with unique colors
+for i, (chromosome, positions) in enumerate(chrom_pos_dict.items()):
+    color = palette[i]
+    plt.scatter([int(chromosome)] * len(positions), positions, color=color, label='Chromosome ' + chromosome)
+
+# Add ellipses for chromosome halves
+for i, (chrom, end) in enumerate(end_data):
+    centromere_position = next(cen for chr_name, cen in chrcen_data if chr_name == chrom)
+
+    # Add ellipse for the first half of the chromosome (from 0 to centromere)
+    ax.add_patch(
+        Ellipse((int(chrom), centromere_position / 2), 0.27, centromere_position, edgecolor='black', linewidth=0.4,
+                fill=False, zorder=20)
+    )
+
+    # Add ellipse for the second half of the chromosome (from centromere to end)
+    ax.add_patch(
+        Ellipse((int(chrom), (end + centromere_position) / 2), 0.27, end - centromere_position, edgecolor='black',
+                linewidth=0.4, fill=False, zorder=20)
+    )
+
+# Add centromere diamonds
 for chrom, cen in chrcen_data:
-    ax.plot([chrom], [cen], marker='D', markersize=5, markerfacecolor="grey", color='black')
+    ax.plot([int(chrom)], [cen], marker='D', markersize=5, markerfacecolor="grey", color='grey', zorder=21)
 
-# Add the genes data as text labels with marks
+# Add genes data as text labels with marks
 for chrom, pos, gene in genes_data:
-    x, y = chrom, pos
-    x_end, y_end = chrom, pos
-    # ax.plot([x, x_end], [y, y_end], color='blue')
-    ax.annotate(gene, xy=(x, y), ha='center', va='center', fontsize=11.5, fontweight='bold',
+    x, y = int(chrom), pos
+    ax.annotate(gene, xy=(x, y), ha='center', va='center', fontsize=11, fontweight='bold', zorder=22,
                 arrowprops=dict(arrowstyle="<-", color='red', facecolor='none', linewidth=0))
 
-# Add the chromend data as marks on the plot
-for chrom, end in end_data:
-    end_pos = get_chrom_end(chrom)
-    if end_pos is not None:
-        plt.plot([chrom], [end_pos], marker='_', markersize=10, color='black')
-
+# Customize plot details
 plt.xlabel('Chromosome', fontsize=18, fontweight='bold')
 plt.ylabel('Position', fontsize=18, fontweight='bold')
-plt.title('SNPEFF & SNPSIFT High confidence p= < 0.05 - Ovaries temperature vs control', fontsize=20, fontweight='bold')
-# Remove the legend by setting an empty label
-plt.plot([], [], label='', color='none')  # Adjust the color as needed
-# #plt.legend(loc='center left', bbox_to_anchor=(1, 0.5)) # to have a legend of the chromosome colors on the right
-plt.tight_layout()  # Adjust the layout to avoid overlapping
-plt.xticks(fontsize=18, fontweight='bold')
+plt.title('SNPEFF & SNPSIFT High Impact = < 0.05 - Ovaries temperature vs control', fontsize=20, fontweight='bold')
+plt.plot([], [], label='', color='none')  # Remove legend by setting an empty label
+plt.tight_layout()
+plt.xticks(range(1, 26), [chrom for chrom, _ in end_data], fontsize=18, fontweight='bold')
 plt.yticks(fontsize=18, fontweight='bold')
 
-
-# save the file
-output_file = 'female_high_filt_sig_snpsift_snpeff.png'  # Replace with your desired filename and extension
+# Save and display the plot
+output_file = 'female_highimpact_snpeffsift_with_ellipses_and_centromeresrocket.png'
 plt.savefig(output_file)
 plt.show()
